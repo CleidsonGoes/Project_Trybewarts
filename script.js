1cadc67e5d45b5fca63bba1f7345c1ded75f8dd2
CleidsonGoes <jorgegoes4@gmail.com>
2023-04-29 12:55:31 -0300

    Requisito 18 feito

--- script.js
+++ script.js
@@ -1,6 +1,16 @@
 const email = document.querySelector('#email');
 const password = document.querySelector('#password');
 
+const btnSubmit = () => {
+  const ckbox = document.querySelector('#agreement');
+  const submitBtn = document.querySelector('#submit-btn');
+  if (ckbox.checked) {
+    submitBtn.disabled = false;
+  } else {
+    submitBtn.disabled = true;
+  }
+};
+
 const validationInput = () => {
   if (email.value === 'tryber@teste.com' && password.value === '123456') {
     alert('Olá, Tryber!');
@@ -12,4 +22,8 @@ const validationInput = () => {
 window.onload = () => {
   const btnEntrar = document.querySelector('#btn');
   btnEntrar.addEventListener('click', validationInput);
+
+  document.getElementById('submit-btn').disabled = true;
+  const checkbox = document.querySelector('#agreement');
+  checkbox.addEventListener('click', btnSubmit);
 };
